ffe5ddedd1f8a03ca84d6d75846d101ccf37852d
guansaipeng <cgsp740178088@163.com>
2019-08-15 17:11:33 +0800

    重载

--- src/9.fun.js
+++ src/9.fun.js
@@ -3,13 +3,31 @@
  * @Author: John.Guan
  * @Date: 2019-08-14 19:11:27
  * @Last Modified by: John.Guan
- * @Last Modified time: 2019-08-15 11:51:01
+ * @Last Modified time: 2019-08-15 15:47:59
  */
-function buildName3(firstName) {
-    var rest = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        rest[_i - 1] = arguments[_i];
+var pukes = {
+    huapais: ['红心', '黑桃', '草花', '方片'],
+    cards: Array(52),
+    createCard: function () {
+        var _this = this;
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedHuapaiIndex = Math.floor(pickedCard / 13);
+            return {
+                huapai: _this.huapais[pickedHuapaiIndex],
+                card: pickedCard % 13
+            };
+        };
     }
-    return firstName + ' ' + rest.join(' ');
+};
+var cardPicker = pukes.createCard();
+// console.log(cardPicker())
+function a(num) {
+    console.log(this);
+    console.log(num);
 }
-console.log(buildName3('guan', '1', '2'));
+a(5); // 打印global和5
+
+interface {
+    add:
+}
--- src/9.fun.ts
+++ src/9.fun.ts
@@ -1,13 +1,27 @@
 /*
- * @Desc: 函数
+ * @Desc:
  * @Author: John.Guan
- * @Date: 2019-08-14 19:11:27
+ * @Date: 2019-08-15 16:23:57
  * @Last Modified by: John.Guan
- * @Last Modified time: 2019-08-15 11:51:01
+ * @Last Modified time: 2019-08-15 17:11:22
  */
 
-function buildName3(firstName: string, ...rest: Array<string>) {
-  return firstName + ' ' + rest.join(' ')
+function pickCard(x: { suit: string; card: number }[]): number
+function pickCard(x: number): { suit: string; card: number }
+
+function pickCard(x): any {
+  // 如果喂入数组，就返回数字
+  if (Array.isArray(x)) {
+    return 1
+  }
+  // 如果喂入数字，就返回对象i
+  if (typeof x === 'number') {
+    return {
+      suit: '红心',
+      card: 1
+    }
+  }
 }
 
-console.log(buildName3('guan', '1', '2')) // 'guan 1 2'
+pickCard(1)
+pickCard([])
